0722b0e65b20141fb4d6d508d08483fd7a543463
Tato2203 <arman2203@hotmail.com>
2022-08-09 18:23:45 -0600

    intefaz semicompleta

--- Proyecto Carro Automatizado/Interfaz/Interfaz.py
+++ Proyecto Carro Automatizado/Interfaz/Interfaz.py
@@ -1,23 +1,29 @@
 import csv
-from tkinter import * 
+from tkinter import *
 from tkinter import filedialog
 
-raiz=Tk()
+raiz = Tk()
+
 
 def AbrirArchivo():
-	archivo = filedialog.askopenfilename(title= "Seleccione el mapa", initialdir = "C:/Users/usuario/Desktop", filetypes = ((" Archivos de Texto","*.txt"),(" Archivos de Archivo de valores separados por comas de Microsoft Excel","*.csv"),(" Todos los Archivos","*.*")))
-	print(archivo)
-	AbrirCSV(archivo)
+    archivo = filedialog.askopenfilename(title="Seleccione el mapa", initialdir="C:/Users/usuario/Desktop", filetypes=(
+        (" Archivos de Archivo de valores separados por comas de Microsoft Excel", "*.csv"), (" Archivos de Texto", "*.txt"), (" Todos los Archivos", "*.*")))
+    print(archivo)
+    AbrirCSV(archivo)
 
-Button (raiz, text= "Abrir archivo", command = AbrirArchivo).pack()
+
+Button(raiz, text="Abrir archivo", command=AbrirArchivo).pack()
+
 
 def AbrirCSV(mapa):
-	with open (mapa) as f: 
-		reader = csv.reader(f, delimiter=";")
-		print (f)
-		for row in reader: 
-			#for columns in row
-				#if 
-			print ("Ap paterno: {0}, Ap materno: {1}, Nombre: {2}, Ciudad: {3}".format(row[0], row[1], row[2], row[3]))
+    with open(mapa) as f:
+        reader = csv.reader(f, delimiter=";")
+        print(f)
+        for row in reader:
+            # for columns in row
+            # if
+            print("{0}, {1}, {2}, {3}, {4}".format(
+                row[0], row[1], row[2], row[3], row[4]))
+
 
 raiz.mainloop()
