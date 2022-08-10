ad3f3e9173be444124251f88357bb6de0ce8b1e9
Tato2203 <arman2203@hotmail.com>
2022-08-09 18:00:53 -0600

    se agregó el Ejemplo de rutas

--- Proyecto Carro Automatizado/Interfaz/Interfaz.py
+++ Proyecto Carro Automatizado/Interfaz/Interfaz.py
@@ -1,11 +1,23 @@
+import csv
 from tkinter import * 
 from tkinter import filedialog
+
 raiz=Tk()
 
 def AbrirArchivo():
 	archivo = filedialog.askopenfilename(title= "Seleccione el mapa", initialdir = "C:/Users/usuario/Desktop", filetypes = ((" Archivos de Texto","*.txt"),(" Archivos de Archivo de valores separados por comas de Microsoft Excel","*.csv"),(" Todos los Archivos","*.*")))
 	print(archivo)
+	AbrirCSV(archivo)
 
 Button (raiz, text= "Abrir archivo", command = AbrirArchivo).pack()
+
+def AbrirCSV(mapa):
+	with open (mapa) as f: 
+		reader = csv.reader(f, delimiter=";")
+		print (f)
+		for row in reader: 
+			#for columns in row
+				#if 
+			print ("Ap paterno: {0}, Ap materno: {1}, Nombre: {2}, Ciudad: {3}".format(row[0], row[1], row[2], row[3]))
 
 raiz.mainloop()
