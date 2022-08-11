73b7dcee12a0e366c06f512a541cd8c797b94f93
Kevin <kevsolano000@gmail.com>
2022-08-10 20:14:48 -0600

    unificación códigos

--- Proyecto Carro Automatizado/Interfaz/Interfaz.py
+++ Proyecto Carro Automatizado/Interfaz/Interfaz.py
@@ -32,5 +32,53 @@ def mostrarMatriz():
 	for i in range (0, len(Matriz)):
 		print ('fila:', i, Matriz[i])
 
+#Funcion auxiliar para detectar el color de la casilla
+def detectar_color(valor_casilla_excel_read):
+        if type(valor_casilla_excel_read) == int:
+                color = 0;
+                if ((valor_casilla_excel_read//10)==1):
+                        color = "verde";
+                        return color
+                elif ((valor_casilla_excel_read//10)==2):
+                        color = "rojo";
+                        return color
+                elif ((valor_casilla_excel_read//10)==3):
+                        color = "gris";
+                        return color
+                elif ((valor_casilla_excel_read//10)==4):
+                        color = "morado";
+                        return color
+                elif ((valor_casilla_excel_read//10)==9):
+                        color = "azul";
+                        return color
+                else:
+                        return "color no detectado"
+        else:
+                return "valor casilla no es int"
+
+#Funcion auxiliar para detectar la direccion de los pasos segun el valor de la casilla
+def direccion_pasos(valor_casilla_excel_read):
+        if type(valor_casilla_excel_read) == int:
+                direccion = 0;
+                if ((valor_casilla_excel_read%10)==0):
+                        direccion = "arriba";
+                        return direccion
+                elif ((valor_casilla_excel_read%10)==1):
+                        direccion = "derecha";
+                        return direccion
+                elif ((valor_casilla_excel_read%10)==2):
+                        direccion = "izquierda";
+                        return direccion
+                elif ((valor_casilla_excel_read%10)==3):
+                        direccion = "abajo";
+                        return direccion
+                else:
+                        return "dirección no detectada"
+        else:
+                return "valor casilla no es un int"
+
+
+
+
 
 raiz.mainloop()
