177ad2b1404cbe19edb43559e4c03ab351e4cc1f
Tato2203 <arman2203@hotmail.com>
2022-10-11 19:13:15 -0600

    modificación del código Interfaz, habían líneas extrañas

--- Proyecto Carro Automatizado/Interfaz/Interfaz.py
+++ Proyecto Carro Automatizado/Interfaz/Interfaz.py
@@ -5,13 +5,10 @@ from tkinter import filedialog
 #se definen las variables con las que se va a trabajar
 raiz = Tk()
 Matriz = [[]]
-<<<<<<< HEAD
 fila = 0 
 columna = 0
 casilla = 0
-=======
- 
->>>>>>> a753d119e6568e35ba3fc1fb12ed67ac86c65499
+
 
 #Función que permite la búsqueda del archivo csv que contiene el mapa a ejecutar
 def AbrirArchivo():
@@ -23,26 +20,23 @@ Button(raiz, text="Abrir archivo", command=AbrirArchivo).pack()
 
 #Función que el cvs y lo traslada a una matriz
 def AbrirCSV(mapa):
-<<<<<<< HEAD
     with open(mapa) as f:
         reader = csv.reader(f, delimiter=";")
         for row in reader:
             Matriz.append(row)
             print (row)
         encontrarInicio()
-=======
-	with open(mapa) as f:
-		reader = csv.reader(f, delimiter=";")
-		for row in reader:
-			Matriz.append(row)
-		print ("Se cerro la interfaz") 
-	mostrarMatriz()
+
+    with open(mapa) as f:
+        reader = csv.reader(f, delimiter=";")
+        for row in reader:
+            Matriz.append(row)
+            print ("Se cerro la interfaz") 
+    mostrarMatriz()
 		
->>>>>>> a753d119e6568e35ba3fc1fb12ed67ac86c65499
 
 #Función que permite desplegar los valores que se leyeron
 def mostrarMatriz():
-<<<<<<< HEAD
     for fila in range (0, len(Matriz)):
         for columna in range (0, len(Matriz[fila])):
             if (Matriz[fila][columna]) == '':
@@ -103,19 +97,17 @@ def encontrarInicio():
             for columna in range(0, len(Matriz[fila])):
                 if (Matriz[fila][columna]) >= '90':
                         print ('El incio del recorrido es en la:', 'fila', fila, 'columna', columna)
-=======
 	#for fila in range (0, len(Matriz)):
 	#	for columna in range (0, len(Matriz[fila])):
 	#		sacarElementos(fila, columna)
 			
-	for i in range (0, len(Matriz)):
-		print ('fila:', i, Matriz[i])
+    for i in range (0, len(Matriz)):
+        print ('fila:', i, Matriz[i])
 
 #Función que permite extraer los valores de la matriz
 def sacarElementos(fila):
 	#print (Matriz[fila][columna])
 	return (Matriz[fila])
 	
->>>>>>> a753d119e6568e35ba3fc1fb12ed67ac86c65499
 
 raiz.mainloop()
