6ebc690e4aff8ce5203f9f94ccdac2d95de157f7
Tato2203 <99569490+Tato2203@users.noreply.github.com>
2022-11-25 16:16:17 -0600

    se quitaron tildes

--- Proyecto Carro Automatizado/Programación/Modo1_Seguimiento_ruta.py
+++ Proyecto Carro Automatizado/Programación/Modo1_Seguimiento_ruta.py
@@ -4,9 +4,16 @@ import os
 import sys 
 import serial 
 
+Matriz_trabajo = [[]]
+color = 0
+direccion = 0
+#se establece la comunicacion serial
+#ser = serial.Serial('/dev/ttyUSB0',9600, timeout=1)
+#ser.flush()
+
 #se definen las variables con las que se va a trabajar
 def paso_siguiente(fila, columna):
-    #mover el motor en la dirección correcta
+    #mover el motor en la direccion correcta
     celda_actual = int(Matriz_trabajo[fila][columna])
     color = detectar_color(celda_actual)
     direccion = direccion_pasos(celda_actual)
@@ -89,7 +96,7 @@ def paso_siguiente(fila, columna):
 
 
 #Funcion auxiliar para detectar la direccion de los pasos segun el valor de la casilla
-def verificación_celda(direccion, fila, columna):
+def verificacion_celda(direccion, fila, columna):
 
     if direccion == 'arriba':
         if detectar_color(Matriz_trabajo[fila-1][columna]) == 'gris':
@@ -112,22 +119,10 @@ def verificación_celda(direccion, fila, columna):
         #ser.write(b"chop\n")
 
     if detectar_color(Matriz_trabajo[fila-1][columna]) == 'gris':
-
-Matriz_trabajo = [[]]
-color = 0
-
-#se establece la comunicación serial
-#ser = serial.Serial('/dev/ttyUSB0',9600, timeout=1)
-#ser.flush()
-
-direccion = 0
 ##
 
-
-
-
 def MORADO(direccion,fila,columna):
-    #avanzar hasta que se encuentre un cero, un borde, en la dirección respectiva
+    #avanzar hasta que se encuentre un cero, un borde, en la direccion respectiva
         if direccion == 'arriba':
             while fila>0:
                 # ser.write(b"direccion\n")
@@ -152,16 +147,16 @@ def MORADO(direccion,fila,columna):
                 # ser.write(b"direccion\n")
                 columna = columna + 1
             paso_siguiente(fila, columna)
-#Función auxiliar de AbrirCSV que permite traer la matriz de Interfaz.py
+#Funcion auxiliar de AbrirCSV que permite traer la matriz de Interfaz.py
 
 def extraerMatriz():
-    for fila in range (1, 6): #este rango definido permite validar que el mapa no se extienda de los parámetros establecidos
-        nuevo_valor = sacarElementos(fila)  #se obtienen la filas de la matriz extraída
+    for fila in range (1, 6): #este rango definido permite validar que el mapa no se extienda de los parametros establecidos
+        nuevo_valor = sacarElementos(fila)  #se obtienen la filas de la matriz extraida
         Matriz_trabajo.append(nuevo_valor)
     print ('traslado de matriz completado')
-    for fila in range (0, len(Matriz_trabajo)):  #permite la comprobación del mapa
+    for fila in range (0, len(Matriz_trabajo)):  #permite la comprobacion del mapa
         print ('fila: ', fila, 'valor:', Matriz_trabajo[fila])
-#Función se encarga de decodificar str a un int, para poder procesar la información
+#Funcion se encarga de decodificar str a un int, para poder procesar la informacion
 
 def deteccionSTRtoINT():
     #se realiza un barrido de la matriz buscando los elementos que deben ser convertidos
@@ -170,14 +165,14 @@ def deteccionSTRtoINT():
             if (Matriz_trabajo[fila][columna]) == '':
                 pass;
             else:
-                #se encuentra un celda que no está vacía y convierte el valor de lo que haya en esta
+                #se encuentra un celda que no esta vacia y convierte el valor de lo que haya en esta
                 valorAconvetir=int(Matriz_trabajo[fila][columna])
                 #print(type(valorAconvetir))
                 #print(valorAconvetir)
-                #se envía el valor convertido para ser procesado e identificar el color y la dirección
+                #se envia el valor convertido para ser procesado e identificar el color y la direccion
                 #print(detectar_color(valorAconvetir))
                 #print(direccion_pasos(valorAconvetir))
-#Función se encarga de obtener la ubicación del inicio del mapa y la retorna
+#Funcion se encarga de obtener la ubicacion del inicio del mapa y la retorna
 
 def encontrarInicio():
     for fila in range (0, len(Matriz_trabajo)):
@@ -240,11 +235,11 @@ def direccion_pasos(valor_casilla_excel_read):
         #ser.write(b"chop\n")
 
 
-#Inicio del código se pide un mapa através de un CSV y la función main
+#Inicio del codigo se pide un mapa atraves de un CSV y la funcion main
 def __main__():
-    extraerMatriz() #esta función se encarga de obtener la matriz leída en la interfaz *siempre debe llamarse para que funcione el código*
-    deteccionSTRtoINT() #esta función se encarga de decodificar los valores que se encuentran en las celdas
-    filaIni, columnaIni = encontrarInicio()  #esta función se encarga de obtener la ubicación del inicio del mapa, este siempre inicia con 9
+    extraerMatriz() #esta funcion se encarga de obtener la matriz leida en la interfaz *siempre debe llamarse para que funcione el codigo*
+    deteccionSTRtoINT() #esta funcion se encarga de decodificar los valores que se encuentran en las celdas
+    filaIni, columnaIni = encontrarInicio()  #esta funcion se encarga de obtener la ubicacion del inicio del mapa, este siempre inicia con 9
     paso_siguiente(filaIni, columnaIni)
 __main__()
-#Funcion auxiliar para detectar la siguiente instrucción a ejecutar
+#Funcion auxiliar para detectar la siguiente instruccion a ejecutar
